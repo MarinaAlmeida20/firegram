0d3c618c5f49182c451dcd865328dc3571aa3204
Marina <marinabrito.ma@gmail.com>
2020-08-04 20:11:37 +0100

    motion

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Modal = ({ selectedImg, setSelectedImg }) => {
 
     const handleClick = (e) => {
-        setSelectedImg(null);
+        if(e.target.classList.contains('backdrop')){
+            setSelectedImg(null);
+        }
+        
     }
 
     return (
-        <div className="backdrop" onClick={handleClick}>
-            <img src={ selectedImg } alt="enlarged pic" />
-        </div>
+        <motion.div className="backdrop" onClick={handleClick}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+        >
+            <motion.img src={ selectedImg } alt="enlarged pic" 
+                initial={{ y: "-100vh" }}
+                animate={{ y: 0 }}
+            />
+        </motion.div>
     )
 }
 
